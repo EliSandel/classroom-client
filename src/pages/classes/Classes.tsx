afb3647a51f0eb08474750648179e7f7c2a9ba7c
Eli Sandel <eli@bestjewishguide.com>
2024-11-12 09:57:19 +0200

    set up files and the routes

---
+++ src/pages/classes/Classes.tsx
@@ -0,0 +1,9 @@
+const Classes = () => {
+    return (
+      <div>
+        <h1>Classse page</h1>
+      </div>
+    )
+  }
+  
+  export default Classes
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter , Route, Routes } from "react-router-dom";
+
+import Create from "../pages/create/Create";
+import Classes from "../pages/classes/Classes";
+import Students from "../pages/students/Students";
+
+const AppRoutes: React.FC = () => {
+  return (
+    <BrowserRouter>
+       <Routes>
+        <Route path="/" element={<Classes />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/create" element={<Create />} />
+       </Routes>
+    </BrowserRouter>
+  )
+};
+
+export default AppRoutes;
