9b09b436e2aeb4220a7957e35bf8974f34f41824
Eli Sandel <eli@bestjewishguide.com>
2024-11-21 16:40:48 +0200

    checj

---
+++ src/components/StudentsTable/StudentsTable.tsx
@@ -0,0 +1,68 @@
+import { Box, Button } from "@mui/material";
+import Table from "@mui/material/Table";
+import Paper from "@mui/material/Paper";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import TableRow from "@mui/material/TableRow";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableContainer from "@mui/material/TableContainer";
+import { IStudent } from "../../interfaces/student.interface";
+import { useStyles } from "./StudentsTable.style";
+
+const StudentsTable = () => {
+  const classes = useStyles();
+  const studentsState: IStudent[] = useSelector(
+    (state: RootState) => state.students.students
+  );
+
+  return (
+    <Box className={classes.root}>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell className={classes.alignCenter}>ID</TableCell>
+              <TableCell className={classes.alignCenter}>First Name</TableCell>
+              <TableCell className={classes.alignCenter}>Last Name</TableCell>
+              <TableCell className={classes.alignCenter}>Age</TableCell>
+              <TableCell className={classes.alignCenter}>Profession</TableCell>
+              <TableCell className={classes.alignCenter}>Assign</TableCell>
+              <TableCell className={classes.alignCenter}>Delete</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {studentsState.map((student) => (
+              <TableRow key={student.id}>
+                <TableCell className={classes.alignCenter}>
+                  {student.id}
+                </TableCell>
+                <TableCell className={classes.alignCenter}>
+                  {student.firstName}
+                </TableCell>
+                <TableCell className={classes.alignCenter}>
+                  {student.lastName}
+                </TableCell>
+                <TableCell className={classes.alignCenter}>
+                  {student.age}
+                </TableCell>
+                <TableCell className={classes.alignCenter}>
+                  {student.profession}
+                </TableCell>
+                <TableCell className={classes.alignCenter}>
+                  <Button variant="outlined">Assign To Class</Button>
+                </TableCell>
+                <TableCell className={classes.alignCenter}>
+                  <Button variant="outlined">Delete</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default StudentsTable;
